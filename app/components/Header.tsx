7161e68712e132deaedfe286eef37ef4a9af078d
anomaly342 <128574773+anomaly342@users.noreply.github.com>
2026-03-14 17:44:16 +0700

    edit font size and padding

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,8 @@ import CartIcon from "./CartIcon.tsx";
 
 export default function Header() {
 	return (
-		<header className="flex justify-between items-center px-4 py-3">
-			<div className="font-semibold text-xl">Store</div>
+		<header className="flex justify-between items-center px-4 py-5 border-b border-b-blue-800">
+			<div className="font-semibold text-2xl">Store</div>
 			<CartIcon></CartIcon>
 		</header>
 	);
