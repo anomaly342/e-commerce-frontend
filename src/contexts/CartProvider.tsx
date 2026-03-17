baa764d0a9dab1498f03965b5ba1d4f79141b9eb
anomaly342 <128574773+anomaly342@users.noreply.github.com>
2026-03-17 12:33:05 +0700

    functional localstorage

--- src/contexts/CartProvider.tsx
+++ src/contexts/CartProvider.tsx
@@ -1,5 +1,5 @@
 import { CartContextType, CartUnit } from "@/types/types.ts";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext<CartContextType>(
 	{} as CartContextType,
@@ -9,6 +9,17 @@ export function CartProvider({ children }: { children: React.ReactNode }) {
 	const [cart, setCart] = useState<CartUnit[]>([]);
 	const [showCart, setShowCart] = useState<boolean>(false);
 
+	useEffect(() => {
+		if (typeof window === undefined) return;
+		const objectString = localStorage.getItem("cart");
+		if (objectString !== null) {
+			setCart(JSON.parse(objectString));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}, [cart]);
 	const { totalPrice, totalQuantity } =
 		cart === undefined
 			? { totalPrice: 0, totalQuantity: 0 }
